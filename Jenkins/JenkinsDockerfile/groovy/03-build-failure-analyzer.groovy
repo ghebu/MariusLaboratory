d18c2fde52662aa69a0ccfe5040e044f906aaab4
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-09-19 14:00:57 +0300

    WIP - Build Failure Analyzer

--- Jenkins/JenkinsDockerfile/groovy/03-build-failure-analyzer.groovy
+++ Jenkins/JenkinsDockerfile/groovy/03-build-failure-analyzer.groovy
@@ -1,29 +1,65 @@
-
-//Docs 
+//DOCS 
 /*
 https://github.com/jenkinsci/build-failure-analyzer-plugin/blob/master/src/test/java/com/sonyericsson/jenkins/plugins/bfa/PluginImplHudsonTest.java
 https://javadoc.jenkins.io/plugin/build-failure-analyzer/com/sonyericsson/jenkins/plugins/bfa/package-summary.html
 */
 
-import com.sonyericsson.jenkins.plugins.bfa.*
 //package com.sonyericsson.jenkins.plugins.bfa
-  
-PluginImpl bfa = new PluginImpl();
-
-println("Is globally enabled: " + bfa.isGlobalEnabled());
-bfa.setGlobalEnabled(true);
-println("Is globally enabled: " + bfa.isGlobalEnabled());
-println("Is instance: " + bfa.isInstance());
-
-bfa.setMaxLogSize(bfa.DEFAULT_MAX_LOG_SIZE);
-bfa.save();
-
-println(bfa.DEFAULT_MAX_LOG_SIZE)
-println("Max log size is: " + bfa.getMaxLogSize())
+import com.sonyericsson.jenkins.plugins.bfa.*;
+import hudson.util.Secret;
+import com.sonyericsson.jenkins.plugins.bfa.db.MongoDBKnowledgeBase;
+import com.sonyericsson.jenkins.plugins.bfa.db.KnowledgeBase;
+import com.sonyericsson.jenkins.plugins.bfa.db.LocalFileKnowledgeBase;
+import com.sonyericsson.jenkins.plugins.bfa.db.MongoDBKnowledgeBase;
 
 
-def knownDatabaseDescriptor = bfa.getKnowledgeBaseDescriptors().findAll { s -> s ==~ /.*Mongo.*/ }[0]
-println(knownDatabaseDescriptor)
 
-bfa.setKnowledgeBase(knownDatabaseDescriptor)
+PluginImpl bfa = new PluginImpl(); 
+PluginImpl instance = PluginImpl.getInstance();
+
+
+def host = "mongo"
+def dbName = "jenkins"
+def userName = null ?: "ghebu"
+def password = null ?: "passw0rd"
+boolean updateMongoConfiguration = false;
+boolean foundLocalFile = false;
+boolean foundMongoDB = false;
+boolean configuredMongoDB = false;
+/*
+public MongoDBKnowledgeBase(String host,
+                            int port,
+                            String dbName,
+                            String userName,
+                            Secret password,
+                            boolean enableStatistics,
+                            boolean successfulLogging)
+*/
+
+
+def kdbDescriptor = bfa.getKnowledgeBaseDescriptor("MongoDBKnowledgeBaseDescriptor");
+def kdb = bfa.getKnowledgeBase();
+println(kdb)
+
+ExtensionList<KnowledgeBase.KnowledgeBaseDescriptor> descriptors =
+                PluginImpl.getInstance().getKnowledgeBaseDescriptors();
+
+
+for (KnowledgeBase.KnowledgeBaseDescriptor descriptor : descriptors) {
+    if (descriptor instanceof LocalFileKnowledgeBase.LocalFileKnowledgeBaseDescriptor) {
+        foundLocalFile = true;
+    } else if (descriptor instanceof MongoDBKnowledgeBase.MongoDBKnowledgeBaseDescriptor) {
+        foundMongoDB = true;
+        mongoDescriptor = descriptor
+    } 
+}
+
+if ((kdbDescriptor == mongoDescriptor) && !updateMongoConfiguration) {
+	mongoKdb = true
+    println(mongoKdb)
+} else {
+    MongoDBKnowledgeBase mongoKB = new MongoDBKnowledgeBase(host,27017, dbName, userName, Secret.fromString("passw0rd"), true, true);
+    instance.setKnowledgeBase(mongoKB);
+    instance.save();
+} 
 
