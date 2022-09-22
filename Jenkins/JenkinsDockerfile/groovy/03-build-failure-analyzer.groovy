0b45f2460147f94ed468c2a3b74e60cf0269d4b3
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-09-22 13:08:25 +0300

    WIP - Build Failure Analyzer

--- Jenkins/JenkinsDockerfile/groovy/03-build-failure-analyzer.groovy
+++ Jenkins/JenkinsDockerfile/groovy/03-build-failure-analyzer.groovy
@@ -11,6 +11,9 @@ import com.sonyericsson.jenkins.plugins.bfa.db.MongoDBKnowledgeBase;
 import com.sonyericsson.jenkins.plugins.bfa.db.KnowledgeBase;
 import com.sonyericsson.jenkins.plugins.bfa.db.LocalFileKnowledgeBase;
 import com.sonyericsson.jenkins.plugins.bfa.db.MongoDBKnowledgeBase;
+import java.util.regex.*
+import com.sonyericsson.jenkins.plugins.bfa.model.FailureCause;
+import com.sonyericsson.jenkins.plugins.bfa.model.indication.BuildLogIndication;
 
 
 
@@ -18,14 +21,20 @@ PluginImpl bfa = new PluginImpl();
 PluginImpl instance = PluginImpl.getInstance();
 
 
-def host = "mongo"
-def dbName = "jenkins"
-def userName = null ?: "ghebu"
-def password = null ?: "passw0rd"
-boolean updateMongoConfiguration = false;
+
+def host = "mongo";
+def dbName = "jenkins";
+def userName = null ?: "ghebu";
+def password = null ?: "passw0rd";
+def port = 27017;
+
+boolean updateMongoConfiguration = true;
 boolean foundLocalFile = false;
 boolean foundMongoDB = false;
 boolean configuredMongoDB = false;
+
+
+
 /*
 public MongoDBKnowledgeBase(String host,
                             int port,
@@ -39,27 +48,47 @@ public MongoDBKnowledgeBase(String host,
 
 def kdbDescriptor = bfa.getKnowledgeBaseDescriptor("MongoDBKnowledgeBaseDescriptor");
 def kdb = bfa.getKnowledgeBase();
-println(kdb)
+
+def isMongoConfigured = kdb ==~ /.*MongoDBKnowledgeBase.*/
 
 ExtensionList<KnowledgeBase.KnowledgeBaseDescriptor> descriptors =
                 PluginImpl.getInstance().getKnowledgeBaseDescriptors();
-
 
 for (KnowledgeBase.KnowledgeBaseDescriptor descriptor : descriptors) {
     if (descriptor instanceof LocalFileKnowledgeBase.LocalFileKnowledgeBaseDescriptor) {
         foundLocalFile = true;
     } else if (descriptor instanceof MongoDBKnowledgeBase.MongoDBKnowledgeBaseDescriptor) {
         foundMongoDB = true;
-        mongoDescriptor = descriptor
+        mongoDescriptor = descriptor;
     } 
 }
 
-if ((kdbDescriptor == mongoDescriptor) && !updateMongoConfiguration) {
-	mongoKdb = true
-    println(mongoKdb)
-} else {
-    MongoDBKnowledgeBase mongoKB = new MongoDBKnowledgeBase(host,27017, dbName, userName, Secret.fromString("passw0rd"), true, true);
+if (isMongoConfigured && !updateMongoConfiguration) {
+	mongoKdb = true;
+    println("MongoDB is already configured and it will not be updated.");
+} else if (isMongoConfigured && updateMongoConfiguration){ 
+    MongoDBKnowledgeBase mongoKB = new MongoDBKnowledgeBase(host, port, dbName, userName, Secret.fromString("passw0rd"), true, true);
     instance.setKnowledgeBase(mongoKB);
     instance.save();
+    println("MongoDB was configured as " + kdb);
 } 
+
+
+
+
+
+
+//Defining Cause
+def causeName = "IsADirectory"
+def causeDescription = "Checking if the file is a directory or not"
+def causeComment = "You are trying to read a directory instead of a file"
+def causeCategory = "BASH"
+def causeLogFilter = "cat: .*: Is a directory"
+
+//FailureCause(String name, String description, String comment, String categories)
+
+FailureCause cause = new FailureCause(causeName, causeDescription, causeComment, causeCategory );
+cause.addIndication(new BuildLogIndication(causeLogFilter));
+cause = kdb.addCause(cause);
+
 
