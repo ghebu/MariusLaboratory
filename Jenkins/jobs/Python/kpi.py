847f9e605a618d4f28b86d63b2bd400684d00eea
Marius Ghebuta <mariusghebuta@192.168.1.7>
2023-03-09 18:56:27 +0200

    Updating the Python script

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -5,17 +5,25 @@ from jenkinsapi.jenkins import Jenkins
 username = 'ghebu'
 password = 'passw0rd'
 url = 'http://localhost:8080'
+server = Jenkins(url, username, password)
 
-def getSCMInfroFromLatestGoodBuild():
-    server = Jenkins(url, username, password)
+
+
+def get_jobs():
     jobs = server.keys()
     pprint(jobs)
-    
     #pprint(dir(server))
-    
-    # jobs = J[jobName]
-    # lgb = job.get_last_good_build()
-    # return lgb.get_revision()
+
+    return jobs
+
+def get_scm_info_from_latest_successful_build():
+    jobs = get_jobs()
+
+    for job in jobs:
+        job = server[job]
+        lgb = job.get_last_good_build()
+        print(lgb.get_revision())
+
 
 if __name__ == '__main__':
-    print(getSCMInfroFromLatestGoodBuild())
+    print(get_scm_info_from_latest_successful_build())
