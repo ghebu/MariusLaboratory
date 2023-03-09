b6205356555b47fcc8fb49a373624edd4923577c
Marius Ghebuta <mariusghebuta@192.168.1.7>
2023-03-09 18:36:35 +0200

    Updating the Python script

---
+++ Jenkins/jobs/Python/kpi.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+import jenkins
+
+username = 'ghebu'
+password = 'passw0rd'
+url = 'http://localhost:8080'
+def getSCMInfroFromLatestGoodBuild():
+    server = Jenkins(url, username, password)
+    print(server)
+    pprint(dir(server))
+    # jobs = J[jobName]
+    # lgb = job.get_last_good_build()
+    # return lgb.get_revision()
+
+if __name__ == '__main__':
+    print getSCMInfroFromLatestGoodBuild('http://localhost:8080')
