8f45f0dfdc9c90df13249d795549789bb9fe6494
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-10 10:08:27 +0200

    Updating the python script kpi.py

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 #https://jenkinsapi.readthedocs.io/en/latest/index.html
 from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.build import Build
 
 username = 'ghebu'
 password = 'passw0rd'
@@ -22,15 +23,21 @@ def get_scm_info_from_latest_successful_build():
     for job in jobs:
         try: 
             build = server[job]
-
-            print(f"the job {job} has the builds {[b for b in build.get_build_ids()]}")
-
+            list_of_builds = [[b for b in build.get_build_ids()]]
+            print(f"the job {job} has the builds {list_of_builds}")
+            get_build_info(job, list_of_builds)
             lgb = build.get_last_good_build()
             git_url = lgb._get_git_repo_url()
             revision = lgb.get_revision()
             print(f"The build {build} with the # {lgb} has the revision {revision} and the git url {git_url}")
         except Exception as e:
             continue 
+
+def get_build_info(job, builds=[]): 
+    
+    for build_no in builds: 
+        build = Build(url, build_no, job)
+        pprint(build)
        
 
 
