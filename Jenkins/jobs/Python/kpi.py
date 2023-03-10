6f37f3276a1e1f750073a2b40895867be543bbcf
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-10 11:19:48 +0200

    Updating the python script kpi.py

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -54,8 +54,10 @@ def get_build_info():
                             headers={jenkins_crumb['crumbRequestField'] : jenkins_crumb['crumb']}).json()
     
     build_date = strftime('%Y-%m-%d %H:%M:%S', localtime(response['timestamp']))
+    
+    pprint(response)
     pprint(build_date)
-
+    
 
 if __name__ == '__main__':
     print(get_scm_info_from_latest_successful_build())
