d1746fc249e060171e4572d3d411b9cf4ebe38e9
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-10 11:01:25 +0200

    Updating the python script kpi.py

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -49,8 +49,8 @@ def get_build_info():
     
     response = requests.get('http://localhost:8080/job/python/job/python-kpi/17/api/json', 
                             auth=(username, password),
-                            headers={jenkins_crumb['crumbRequestField'] : jenkins_crumb['crumb']})
-    pprint(response.content)
+                            headers={jenkins_crumb['crumbRequestField'] : jenkins_crumb['crumb']}).json()
+    pprint(response)
 
 
 if __name__ == '__main__':
