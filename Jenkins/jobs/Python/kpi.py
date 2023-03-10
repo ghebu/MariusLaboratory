cd26bfe69e80a4b220936434af3f69961670505e
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-10 09:50:16 +0200

    Updating the python script kpi.py

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -23,7 +23,7 @@ def get_scm_info_from_latest_successful_build():
         try: 
             build = server[job]
 
-            pprint(dir(build))
+            pprint(build.get_build_ids())
 
             lgb = build.get_last_good_build()
             git_url = lgb._get_git_repo_url()
