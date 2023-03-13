b5853c736d1654751168e0359d2b28ce0c4f340f
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-13 09:22:53 +0200

    Removing the Build object

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -11,8 +11,8 @@ password = 'passw0rd'
 jenkins_url = 'http://localhost:8080'
 server = Jenkins(jenkins_url, username, password)
 
-build_obj = Build('http://localhost:8080/', 17, 'python-kpi')
-pprint(f"BUILD INFO: {build_obj}")
+# build_obj = Build('http://localhost:8080/', 17, 'python-kpi')
+# pprint(f"BUILD INFO: {build_obj}")
 
 jenkins_crumb = requests.get(f'{jenkins_url}/crumbIssuer/api/json', auth=(username, password)).json()
 
