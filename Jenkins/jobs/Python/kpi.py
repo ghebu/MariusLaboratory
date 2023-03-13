d62668fed61b15962987c073b4faa6e9d0cb406d
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-13 09:32:50 +0200

    Removing the Build object

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -34,7 +34,7 @@ def get_scm_info_from_latest_successful_build():
             print(f"the job {job} has the builds {list_of_builds}")
             
             #pprint(dir(build)) #functions: get_last_buildnumber, http://localhost:8080/job/python/job/python-kpi/19/console
-            print(f"Full name: {build.get_full_name()}")
+            print(f"Full name: {[z for z in server.get_jobs_info()]}")
             
             lgb = build.get_last_good_build()
             git_url = lgb._get_git_repo_url()
