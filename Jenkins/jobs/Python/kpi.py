36a62222097bfd7e25e659b3d27aea7d1fa7d473
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2023-03-14 09:25:21 +0200

    Updating the python script kpi.py

--- Jenkins/jobs/Python/kpi.py
+++ Jenkins/jobs/Python/kpi.py
@@ -76,16 +76,16 @@ def get_build_info(job_url, job_name, list_of_builds, git_url):
         author = response['actions'][0]['causes'][0]['userName'] ##alternative userId can be used.
         duration = response['duration'] / 1000 #ms to seconds transformation
         
-        if 'bitbucket' in git_url: 
-            department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split('/')[5].replace('.git','')
-        elif 'code.connected' in git_url: 
-            department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split(':')[1].split('/')[0].replace('.git','') 
-        elif 'github' in git_url: 
-            department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split(':')[1].split('/')[0].replace('.git','')
+        # if 'bitbucket' in git_url: 
+        #     department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split('/')[5].replace('.git','')
+        # elif 'code.connected' in git_url: 
+        #     department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split(':')[1].split('/')[0].replace('.git','') 
+        # elif 'github' in git_url: 
+        #     department = git_url.split('/')[3].replace('.git','')  if 'http' in git_url else git_url.split(':')[1].split('/')[0].replace('.git','')
 
-        app = git_url.split('/')[4] if git_url.find('https') else git_url.split('/')[5]
+        # app = git_url.split('/')[4] if git_url.find('https') else git_url.split('/')[5]
 
-        pprint(f"department and app: {department}, {app}")
+        # pprint(f"department and app: {department}, {app}")
         payload = {
             'url' : job_url,
             'build' : build,
